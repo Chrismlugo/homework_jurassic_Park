1348787590d231e28b7393d84055eadfefffbaba
Chris Murphy <_chrism_@live.com>
2018-04-03 21:26:44 +0100

    tests can remove all dinos of a certain type from enclosure

--- Park.js
+++ Park.js
@@ -6,4 +6,15 @@ Park.prototype.addDino = function (dino) {
   this.enclosure.push(dino);
 };
 
+Park.prototype.removeDinoType = function (type) {
+  // for(let dinosaur of this.enclosure){
+  //   if(dinosaur.type === type){
+  //     var index = this.enclosure.indexOf(dinosaur);
+  //     this.enclosure.splice(index);
+  //   }
+  // }
+  const result = this.enclosure.filter(dino => dino.type !== type);
+  this.enclosure = result;
+};
+
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 var Park = require('../Park.js');
 var Dino = require('../Dino.js')
 
-describe('Park', function(){
+describe(' Empty Park', function(){
 
 beforeEach(function(){
   park = new Park();
@@ -17,6 +17,24 @@ it('can add dinosaur to enclosure', function(){
   park.addDino(dinosaur1);
   assert.strictEqual(park.enclosure.length, 1);
   assert.deepStrictEqual(park.enclosure, [dinosaur1]);
+});
+});
+
+describe('Park with dinos', function(){
+  beforeEach(function(){
+    park = new Park();
+    dinosaur2 = new Dino("Triceratops", 3);
+    dinosaur3 = new Dino("Triceratops", 3);
+    dinosaur4 = new Dino("T-Rex", 1)
+    park.addDino(dinosaur2);
+    park.addDino(dinosaur3);
+    park.addDino(dinosaur4);
+  });
+
+it('can remove dino of type', function(){
+  park.removeDinoType("T-Rex");
+  assert.strictEqual(park.enclosure.length, 2);
+  assert.deepStrictEqual(park.enclosure, [dinosaur2, dinosaur3]);
 });
 
 });
