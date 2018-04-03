c191c80e6c35b03ea6814fa6f5e43c54dc837a38
Chris Murphy <_chrism_@live.com>
2018-04-03 17:47:07 +0100

    set up jurassic park folder with test specs for dino and park. tests dino has type and has offspring

---
+++ specs/dino_spec.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+var Dino = require('../Dino.js');
+
+
+describe('Dinosaur', function(){
+
+    beforeEach(function(){
+      dino1 = new Dino("Raptor", 2);
+    });
+
+    it('dino has a type', function(){
+      assert.strictEqual(dino1.type, "Raptor");
+    });
+
+    it('has a number of offspring', function(){
+      assert.strictEqual(dino1.offspring, 2);
+    });
+});
